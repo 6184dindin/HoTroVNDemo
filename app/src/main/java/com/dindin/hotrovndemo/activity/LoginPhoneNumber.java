1316ff7f64b032c3c8e095bcb8319e6bd02b1dc3
DinDin <71533874+6184dindin@users.noreply.github.com>
2020-10-25 02:05:56 +0700

    10252020

--- app/src/main/java/com/dindin/hotrovndemo/activity/LoginPhoneNumber.java
+++ app/src/main/java/com/dindin/hotrovndemo/activity/LoginPhoneNumber.java
@@ -1,16 +1,18 @@
 package com.dindin.hotrovndemo.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
 import com.dindin.hotrovndemo.R;
+import com.dindin.hotrovndemo.databinding.ActivityLoginPhoneNumberBinding;
 
 public class LoginPhoneNumber extends AppCompatActivity {
-
+    ActivityLoginPhoneNumberBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_phone_number);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_login_phone_number);
     }
 }
--- app/src/main/java/com/dindin/hotrovndemo/activity/MainActivity.java
+++ app/src/main/java/com/dindin/hotrovndemo/activity/MainActivity.java
@@ -1,16 +1,19 @@
 package com.dindin.hotrovndemo.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 
 import com.dindin.hotrovndemo.R;
+import com.dindin.hotrovndemo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }

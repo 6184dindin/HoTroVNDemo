c5f3fa1d82589b10bf2ae770717e08805cee4c3a
DinDin <71533874+6184dindin@users.noreply.github.com>
2020-10-25 18:53:49 +0700

    10252020

--- app/src/main/java/com/dindin/hotrovndemo/activity/CreateReliefCampaignActivity.java
+++ app/src/main/java/com/dindin/hotrovndemo/activity/CreateReliefCampaignActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.dindin.hotrovndemo.R;
 
-public class ReliefCampaignActivity extends AppCompatActivity {
+public class CreateReliefCampaignActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_relief_campaign);
+        setContentView(R.layout.activity_create_relief_campaign);
     }
 }
